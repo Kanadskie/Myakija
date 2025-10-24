75da41132eacac7947923701df41ee083da7886a
Dmitriy Levykh <da-levikh@ya.ru>
2025-10-24 14:44:19 +0300

    Fix router

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+// Редирект для GitHub Pages
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+if (sessionStorage.redirect) {
+  const redirect = sessionStorage.redirect;
+  delete sessionStorage.redirect;
+  window.location.href = redirect;
+}
+
 root.render(
-  <React.StrictMode>
+  <Router basename="/Myakija">
     <App />
-  </React.StrictMode>
+  </Router>
 );
